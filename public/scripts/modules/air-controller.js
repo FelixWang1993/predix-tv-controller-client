326b215e1ba15948978557731fcfd32633f2e665
ervin <xiaolongtang89@gmail.com>
2016-07-21 23:14:50 -0700

    create basic project

---
+++ public/scripts/modules/air-controller.js
@@ -0,0 +1,6 @@
+	define(['angular', './px-module'], function(angular, sampleModule) {
+	    'use strict';
+	    return sampleModule.controller('AirCtrl', ['$scope','$http', function($scope,$http,$stateParams) {
+
+	    	    }]);
+	});
---
+++ public/scripts/modules/index-controller.js
@@ -0,0 +1,35 @@
+define(['angular', './px-module'], function(angular, sampleModule) {
+    'use strict';
+    return sampleModule.controller('IndexCtrl', ['$scope','$http','$stateParams', function($scope,$http,$stateParams) {
+
+		var el = document.querySelector('#rangepicker');
+        el.addEventListener('range-changed', function(e) {
+            console.log('from = ' + el.range.from);
+            reGetProducts();
+        });
+
+        $scope.paginationConf = {
+            currentPage: 1,
+            totalItems: 10,
+            itemsPerPage: 10
+        };
+
+        
+        var reGetProducts = function(){
+            var postData = JSON.stringify({
+                page: $scope.paginationConf.currentPage,
+                pageSize: $scope.paginationConf.itemsPerPage,
+                from: el.range.from,
+                to: el.range.to
+            });
+            console.log('currentPage = ' + $scope.paginationConf.currentPage);
+
+            $http.post('/api/ms/pi/dht', postData).success(function(data){
+                $scope.paginationConf.totalItems = data.total;
+                $scope.items = data.items;
+                // console.log("data:"+data.items);
+            });
+        };
+        $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage', reGetProducts);
+    }]);
+});
