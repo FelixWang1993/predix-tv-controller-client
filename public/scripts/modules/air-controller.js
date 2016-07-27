30c5937b0451c909499268a47b3ce21567ac40de
ervin <xiaolongtang89@gmail.com>
2016-07-26 17:13:39 -0700

    ait page update

--- public/scripts/modules/air-controller.js
+++ public/scripts/modules/air-controller.js
@@ -11,6 +11,24 @@
 	    		$http.get('/api/mservice/value?type=pm1.0').success(function(data){
 	    			$scope.pm1=data;
 	    		});
+	    		$http.get('/api/mservice/maxmin?type=max&value=pm2.5').success(function(data){
+	    			$scope.pm25max=data;
+	    		});
+	    		$http.get('/api/mservice/maxmin?type=min&value=pm2.5').success(function(data){
+	    			$scope.pm25min=data;
+	    		});
+	    		$http.get('/api/mservice/maxmin?type=max&value=pm1.0').success(function(data){
+	    			$scope.pm1max=data;
+	    		});
+	    		$http.get('/api/mservice/maxmin?type=min&value=pm1.0').success(function(data){
+	    			$scope.pm1min=data;
+	    		});
+	    		$http.get('/api/mservice/maxmin?type=max&value=pm10').success(function(data){
+	    			$scope.pm10max=data;
+	    		});
+	    		$http.get('/api/mservice/maxmin?type=min&value=pm10').success(function(data){
+	    			$scope.pm10min=data;
+	    		});
 	    	};
     	    setInterval(function(){
             $scope.$apply(updateStatus);
